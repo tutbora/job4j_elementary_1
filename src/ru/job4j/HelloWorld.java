8845b4b99ff92d6da3955671e6de647d40c9769d
tutbora <tutbora@gmail.com>
2020-12-06 20:30:51 +0300

    3. Арифметические операции. [#424844]

---
+++ src/ru/job4j/HelloWorld.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j!");
+    }
+}
---
+++ src/ru/job4j/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class HelloWorldTest {
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        HelloWorld.main(null);
+        assertThat(out.toString(), is("Hello, Job4j!" + System.lineSeparator()));
+    }
+
+}
