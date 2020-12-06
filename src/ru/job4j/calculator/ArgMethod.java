6c42e595d77e21fc961e544a75374202746ee2fc
tutbora <tutbora@gmail.com>
2020-12-06 22:31:49 +0300

    4.1. Аргументы [#424842]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,55 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name, String lastName) {
+        System.out.println("Hello, " + name + lastName);
+    }
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", " + age);
+    }
+    public static void hello(int age) {
+        System.out.println("Hello, " + age);
+    }
+    public static void hello(String name, String lastName, String thirdName) {
+        System.out.println("Hello, " + name + lastName + thirdName);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr ";
+        String lastName = "Arsentev ";
+        String thirdName = "Next";
+        int age = 33;
+        
+        ArgMethod.hello(name, lastName);
+
+        ArgMethod.hello(name, age);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name, lastName, thirdName);
+    }
+}
+
+/*
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+
+        ArgMethod.hello();
+
+        ArgMethod.hello(name, age);
+
+        ArgMethod.hello(age);
+
+        ArgMethod.hello(name, name, name);
+    }
+}*/
