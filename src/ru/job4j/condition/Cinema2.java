9f9b85f0a4c8953468715774f88a9be3992c2285
tutbora <tutbora@gmail.com>
2020-12-08 17:27:46 +0300

    4.3.1. Оператор && [#424784]

---
+++ src/ru/job4j/condition/Cinema2.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Cinema2 {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
