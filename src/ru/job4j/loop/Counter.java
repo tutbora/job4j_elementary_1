e04f59e3f5c790905905f5cf1904b9e996e26a4a
tutbora <tutbora@gmail.com>
2020-12-09 21:27:50 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#424896] расширил до отрицательных в диапазоне

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -1,32 +1,17 @@
-/*
 package ru.job4j.loop;
 
 public class Counter {
-    public static int sum(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            sum = sum + i;
-        }
-        return sum;
-    }
 
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for ( ... ) {
-            if ( ... ) {
-                sum = sum + ... ;
+        int startToPositive;
+        for (int i = start; i <= finish; i++) {
+            if (start % 2 == 0) {
+                startToPositive = start;
+                sum = sum + Math.abs(startToPositive);
             }
+            start++;
         }
         return sum;
     }
-
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-
-        System.out.println(sumByEven(0, 10));
-        System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
-    }
-}*/
+}
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int rsl = Counter.sumByEven(1, 10);
+        int expected = 30;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromOneToFortyOneThenThirty() {
+        int rsl = Counter.sumByEven(20, 40);
+        int expected = 330;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromNineteenToFortyThenThreeHundredThirty() {
+        int rsl = Counter.sumByEven(19, 41);
+        int expected = 330;
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromNegativeTenToPositiveTenThenSixty() {
+        int rsl = Counter.sumByEven(-10, 10);
+        int expected = 60;
+        assertThat(rsl, is(expected));
+    }
+}
