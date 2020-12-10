d18668716000c22a0c7bbdfac911b811cb2dea77
tutbora <tutbora@gmail.com>
2020-12-10 12:02:09 +0300

    5.3. Построить шахматную доску в псевдографике. [#424900] много опечаток в условии

---
+++ src/ru/job4j/loop/Board.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        int step = 2;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                /* условие проверки, что писать пробел или X */
+                /* Выше в задании мы определили закономерность,
+                 когда нужно проста,взять X */
+                if (step % 2 == 0) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+                step++;
+            }
+            /* добавляем перевод на новую строку. */
+            System.out.println();
+            step = width % 2 == 0 ? step + 1 : step;
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
