2ce89cdac58dccfd699ca35d3010c1ebbd5ff8dc
tutbora <tutbora@gmail.com>
2020-12-11 10:40:59 +0300

    5.4. Протеиновая диета!Исправил[#424888]

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -24,4 +24,10 @@ public class FitnessTest {
         int month = Fitness.calc(50, 90);
         assertThat(month, is(2));
     }
+
+    @Test
+    public void whenIvanSameByFewNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
+    }
 }
