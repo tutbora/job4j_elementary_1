9f15600969118a0eb1d86811d841bc000d6e4fae
tutbora <tutbora@gmail.com>
2020-12-16 22:27:27 +0300

    6.4. Слово начинается с ...!исправлено2 [#424800]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,11 +3,9 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
-        for (int i = 0; i <= pref.length - 1; i++) {
-            if (word[i] == pref[i]) {
-                result = true;
-            } else {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
                 result = false;
                 break;
             }
