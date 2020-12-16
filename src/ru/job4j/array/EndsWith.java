c03588eeca30541bfa50abfed1c2b333f20e8c94
tutbora <tutbora@gmail.com>
2020-12-16 22:21:19 +0300

    6.4.2. Слово заканчивается на ...!исправлено2 [#424804]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,12 +2,11 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        /* проверить. что массив word имеет последние элементы одинаковые с post */
-        boolean result = false;
+        /* проверить. что массив word имеет последние
+        элементы одинаковые с post */
+        boolean result = true;
         for (int i = 1; i <= post.length; i++) {
-            if (word[word.length - i] == post[post.length - i]) {
-                result = true;
-            } else {
+            if (word[word.length - i] != post[post.length - i]) {
                 result = false;
                 break;
             }
